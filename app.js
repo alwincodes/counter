101c17af959c4121cbe05cd0d3d6e1e7646b2f2d
cyberpunked69 <67334755+cyberpunked69@users.noreply.github.com>
2020-10-14 14:29:09 +0530

    blinking added

--- app.js
+++ app.js
@@ -2,14 +2,24 @@
 const count = document.querySelector(".count");
 const addCount = document.querySelector("#add");
 const delCount = document.querySelector("#del");
+const body = document.querySelector("body")
 //event listeners
 addCount.addEventListener("click",add);
 delCount.addEventListener("click",del);
 //functions
+function colour(num){
+    if(num % 2 == 0){
+         body.classList.add("even");
+    }
+    else{
+        body.classList.remove("even");
+    }
+}
 function add(){
     let num = count.textContent;
     num++;
     count.innerHTML=num;
+    colour(num);
 }
 function del(){
     let num = count.textContent;
@@ -19,4 +29,5 @@ function del(){
         num--;
         count.innerHTML=num;
     }
+    colour(num);
 }
